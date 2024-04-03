48d68e37e4cf82b053e0f90b088655f872d11425
Tausif Ahmad <tausifahmad754@gmail.com>
2024-04-03 19:22:31 +0530

    Day-8 Factorial of a number

---
+++ factorial.js
@@ -0,0 +1,11 @@
+// Factorial of a number
+const factorial = (num) => {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact = fact * i;
+  }
+  return fact;
+};
+console.log(factorial(5));
+console.log(factorial(0));
+console.log(factorial(3));
--- findMax.js
+++ findMax.js
@@ -1,6 +1,8 @@
 // Max in the array
 const findMax = (arr) => {
+  //   return Math.max(10, 20, 100, 600, 2);
   return Math.max(...arr);
 };
+// console.log(findMax());
 console.log(findMax([1, 5, 3, 9, 2]));
-console.log(findMax([-10, -5, -3, -9, 2]));
+console.log(findMax([-10, -5, -3, -9, -2]));
