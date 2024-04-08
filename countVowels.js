3b0555d28d866d688629e4a984ce608f625210d1
Tausif Ahmad <tausifahmad754@gmail.com>
2024-04-08 16:30:20 +0530

    Day-13 count vowels

---
+++ countVowels.js
@@ -0,0 +1,14 @@
+// count number of vowels
+const countVowels = (str) => {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let arr = str.split("");
+  //   console.log(arr);
+  let count = 0;
+  for (let char of arr) {
+    if (vowels.includes(char.toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
+};
+console.log(countVowels("HEllo world"));
