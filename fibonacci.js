7fac388482c880385c0742b7b43fada57a14f612
Tausif Ahmad <tausifahmad754@gmail.com>
2024-04-21 19:34:42 +0530

    Day-25 Fibonacci using Recursion

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+// Fibonacci using Recursion
+
+function fibonacci(num) {
+  if (num <= 1) {
+    return num;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+}
+console.log(fibonacci(5)); //output-5
+console.log(fibonacci(0));
