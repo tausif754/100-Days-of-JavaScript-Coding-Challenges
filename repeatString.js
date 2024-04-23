a661433d4be0775040a293adf9dcf8c5ca636e82
Tausif Ahmad <tausifahmad754@gmail.com>
2024-04-23 12:49:18 +0530

    Day-27 Repeat String

---
+++ repeatString.js
@@ -0,0 +1,7 @@
+// write a function called repeatString that takes two parameters
+
+function repeatString(str, num) {
+  return num > 0 ? str.repeat(num) : str;
+}
+
+console.log(repeatString("Tausif ", 3));
