57cf36ad6e8dbdb986cc192cd63b6f6afd580a49
Tausif Ahmad <tausifahmad754@gmail.com>
2024-05-06 20:50:58 +0530

    Day-38 calculate days between days

---
+++ calculateDaysBetweenDays.js
@@ -0,0 +1,9 @@
+// calculate days between days
+
+const calculateDaysBetweenDays = (d1, d2) => {
+  let date1 = new Date(d1);
+  let date2 = new Date(d2);
+  let diff = Math.abs(date2 - date1);
+  return diff / (24 * 60 * 60 * 1000);
+};
+console.log(calculateDaysBetweenDays("2024-01-01", "2024-01-31"));
