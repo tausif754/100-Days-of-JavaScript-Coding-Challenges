26bb565aad7ef1c860fcecb05af5256179f7cca4
Tausif Ahmad <tausifahmad754@gmail.com>
2024-09-03 19:27:01 +0530

    Added new program

--- calculateDaysBetweenDays.js
+++ calculateDaysBetweenDays.js
@@ -6,4 +6,4 @@ const calculateDaysBetweenDays = (d1, d2) => {
   let diff = Math.abs(date2 - date1);
   return diff / (24 * 60 * 60 * 1000);
 };
-console.log(calculateDaysBetweenDays("2024-01-01", "2024-01-31"));
+console.log(calculateDaysBetweenDays("2024-01-01", "2024-01-31")); //output-30
